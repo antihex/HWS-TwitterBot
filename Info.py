fa3d17d65c648d7491fd44e9f9187776f3b04625
antihex <59896064+antihex@users.noreply.github.com>
2022-03-30 13:39:25 -0400

    Info.py

---
+++ Info.py
@@ -0,0 +1,18 @@
+from asyncio.windows_events import NULL
+from gettext import NullTranslations
+import praw
+
+# personal details for twitter
+consumer_key = NULL
+consumer_secret = NULL
+access_token = NULL
+access_token_secret = NULL
+
+# Reddit Login
+reddit = praw.Reddit(
+    client_id="NULL",
+    client_secret= NULL,
+    user_agent= NULL,
+    username= NULL,
+    password= NULL,
+)
